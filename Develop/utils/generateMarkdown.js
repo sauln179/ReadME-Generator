601bd90dced29328ccc78aef84b4224a2b120c30
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-02 13:21:33 -0700

    Update the markdown to generate readme.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,70 @@
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  //Create constant that store user license selection
+  const licenseChoice = data.license;
+  //To verify
+console.log(licenseChoice);
+//Depending on choice, user gets a generated page, that is clickable.
+function renderLicenseBadge() {
+  if (licenseChoice == "Apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+} else if (licenseChoice == "Boost") {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+} else if (licenseChoice == "GNU") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+} else if (licenseChoice == "ISC") {
+    return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+} else if (licenseChoice == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+} else if (licenseChoice == "Mozilla") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+} 
+else if (licenseChoice == "Open") {
+  return "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)";
+} 
+else {return
+   "";}
+}
+//Returns all user data, and creates the readme.
+//Includes clickable links.
+  return `
+  # ${data.projectName}
+  ## ${data.license}
+  ${renderLicenseBadge()}
+
+
+ 
+  # Description:
+  ${data.description}
+
+  # Table of Content:
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+# Installation
+${data.installation}
+
+# Usage
+${data.usage}
+
+# Contributing
+${data.contributing}
+
+# Test
+${data.tests}
+
+# Questions
+Here is the following information to reach the creator:
+
+- Github Username: [${data.username}](https://github.com/${data.username})
+- Email Address: ${data.email}
+
 `;
+
 }
 
 module.exports = generateMarkdown;
